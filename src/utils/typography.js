cacc31b75dc2fbd17d0f44bad303a95206ba5818
Andrew <aks427@gmail.com>
2019-05-01 23:28:57 -0700

    refactored

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography";
-import kirkhamTheme from "typography-theme-kirkham";
+import theme from "typography-theme-fairy-gates";
 
-const typography = new Typography(kirkhamTheme);
+const typography = new Typography(theme);
 
 export default typography;
 export const rhythm = typography.rhythm;
