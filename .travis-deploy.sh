743d03cc1ab4fc14c5b7eba9c17c787ce669221e
Andrew <aks427@gmail.com>
2019-07-02 20:22:29 -0700

    switch config files back

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -4,7 +4,6 @@ set -e
 # setup ssh-agent and provide the GitHub deploy key
 eval "$(ssh-agent -s)"
 
-
 openssl aes-256-cbc -K $encrypted_f009e2359ef0_key -iv $encrypted_f009e2359ef0_iv -in deploykey.enc -out deploykey -d
 chmod 600 deploykey
 ssh-add deploykey
