1f3d90ca8883b053aae594be44ac92f8dcb32cc2
Andrew <aks427@gmail.com>
2019-07-02 20:04:46 -0700

    try 3

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -3,8 +3,10 @@ set -e
 
 # setup ssh-agent and provide the GitHub deploy key
 eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_f009e2359ef0_key -iv $encrypted_f009e2359ef0_iv -in deploykey.enc -out id_deploy -d
-chmod 600 id_deploy
-ssh-add id_deploy
 
-./node_modules/.bin/gh-pages -d public -b gh-pages
+
+openssl aes-256-cbc -K $encrypted_f009e2359ef0_key -iv $encrypted_f009e2359ef0_iv -in deploykey.enc -out deploykey -d
+chmod 600 deploykey
+ssh-add deploykey
+
+./node_modules/.bin/gh-pages -d public -b gh-pages -r git@github.com:aks427/andrewstevens.dev.git
