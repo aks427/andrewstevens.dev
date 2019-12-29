3a43e668d189fa2e32e0d06d7875e086c995e729
Andrew <aks427@gmail.com>
2019-12-28 18:45:09 -0800

    attempting fix

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { Helmet } from "react-helmet";
 import Layout from "../components/layout";
 import "../styles/books.scss";
@@ -7,14 +7,20 @@ export default () => {
   const numberOfBooks = 18;
   const widgetReadId = "read";
 
+  useLayoutEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = `https://www.goodreads.com/review/grid_widget/9868029?cover_size=medium&hide_link=true&hide_title=true&num_books=${numberOfBooks}&order=d&shelf=read&sort=date_read&widget_id=${widgetReadId}`;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  });
+
   return (
     <Layout>
       <Helmet>
         <title>Books</title>
-        <script
-          type="text/javascript"
-          src={`https://www.goodreads.com/review/grid_widget/9868029?cover_size=medium&hide_link=true&hide_title=true&num_books=${numberOfBooks}&order=d&shelf=read&sort=date_read&widget_id=${widgetReadId}`}
-        />
       </Helmet>
       <div className="books">
         <h2>Recently Read</h2>
